db7144d4b72811c2f5bfe68fc2cc9b95b06e8a4d
Ethan Fredsti <ethan@fredsti.com>
2023-11-16 15:28:18 -0800

    Update ohms_law.rs

--- src/electronics/ohms_law.rs
+++ src/electronics/ohms_law.rs
@@ -1,6 +1,6 @@
 // https://en.wikipedia.org/wiki/Ohm%27s_law
 
-fn onms_law(voltage: f64, current: f64, resistance: f64) -> Result<f64, &str>
+fn onms_law(voltage: f64, current: f64, resistance: f64) -> f64
     {
         /*
         Apply Ohm's Law, on any two given electrical values, which can be voltage, current,
@@ -12,9 +12,15 @@ fn onms_law(voltage: f64, current: f64, resistance: f64) -> Result<f64, &str>
         >>> ohms_law(resistance=0, voltage=-10, current=1) -> {'resistance': -10.0}
         >>> ohms_law(voltage=0, current=-1.5, resistance=2) -> {'voltage': -3.0}
         */
+
+        
+        
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
         if (voltage == 0 && current == 0) || (voltage == 0 && resistance == 0) || (current == 0 && resistance == 0)
         {
             //return an error for more than one variable being zero
+            println!("Exactly one arguement must be 0");
+            return 0;
         }
         if resistance < 0 
         {
@@ -22,15 +28,15 @@ fn onms_law(voltage: f64, current: f64, resistance: f64) -> Result<f64, &str>
         }
         if voltage == 0
         {
-            Ok(current * resistance)
+            current * resistance
         }
         if current == 0
         {
-            Ok(voltage / resistance)
+            voltage / resistance
         }
         if resistance == 0
         {
-            Ok(voltage / current)
+            voltage / current
         }
         else
         {
